7270fab13ca9a70b6a768c2fa3a32d9f826d9baa
YevhenMatvieiev <egmegm01@gmail.com>
2019-08-04 21:28:11 +0300

    update

--- sum_parser.py
+++ sum_parser.py
@@ -1,14 +1,13 @@
 import requests
-from bs4 import BeautifulSoup
-
+import bs4
 
 base_url = 'http://sum.in.ua/?swrd='
-word = 'шевченко'
+word = 'консенсус'
 
 def parsing(word):
     url = base_url + word
     r = requests.get(url).text
-    soup = BeautifulSoup(r, 'lxml')
+    soup = bs4.BeautifulSoup(r, 'lxml')
     article = soup.find_all('div', id='article')
     result = ''
     if len(article) == 0:
@@ -16,12 +15,20 @@ def parsing(word):
         result = article.find('p').text.replace('Можливо, стануть у пригоді: ', '').replace('Можливо, ви шукали:', '')
         result = result.replace('Можливо, вас зацікавить:', '')
     else:
-        for i in article:
-            for j in i.find_all('p'):
-                if j.attrs == {'class': ['tom', 'comm']}:
-                    pass
+        div = soup.find('div', class_="fedbody")
+        if div is not None:
+            for i in div:
+                if isinstance(i, bs4.element.Tag):
+                    result += i.text.replace('\n\n', '\n')
                 else:
-                    result += str(j.text + '\n\n')
+                    result += i
+        else:
+            for i in article:
+                for j in i.find_all('p'):
+                    if j.attrs == {'class': ['tom', 'comm']}:
+                        pass
+                    else:
+                        result += str(j.text + '\n\n')
 
     return result
 
